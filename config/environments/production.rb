6b90e03871a252253343e18058025f8cd283b467
salmansiddiquitp <salman.siddiqui@tenpearls.com>
2013-08-21 14:45:31 +0500

    configured for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ DemoPortal::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { :host => 'mighty-falls-2409.herokuapp.com' }
 end
